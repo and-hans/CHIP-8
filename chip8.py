22bf2c217c3dff0b03089f59bd4718036ba1eb11
Andrew Hansraj <hansrajandrew@gmail.com>
2023-07-26 14:13:12 -0400

    set starting point for pc and sp

--- chip8.py
+++ chip8.py
@@ -1,10 +1,10 @@
 class Chip8:
     def __init__(self):
         self.memory = bytearray(4096)  # memory/RAM
-        self.pc = 0  # program counter
+        self.pc = 0x200  # program counter  (starts at 512 or 0x200)
         # registers
         self.index = 0  # I/index register
-        self.sp = 0  # stack pointer register
+        self.sp = 0x52  # stack pointer register (starts at 82 or 0x52)
         self.timers = {  # delay and sound timers
             'delay' : 0,
             'sound' : 0
@@ -12,5 +12,3 @@ class Chip8:
         self.reg = {}  # stores 16 general purpose registers
         for i in range(16):
             self.reg['v'+str(hex(i))[2:]] = 0
-
-
